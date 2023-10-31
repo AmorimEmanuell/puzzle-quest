309a8b01ef644a5ee915c2f48fffb60e411f13ec
Emanuell André Amorim de Lima <amorimnuell@gmail.com>
2023-10-31 07:00:40 -0300

    Remove unused classes

--- Source/PuzzleQuest/FireBall.h
+++ Source/PuzzleQuest/FireBall.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "FireBall.generated.h"
 
+class UPrimitiveComponent;
+
 UCLASS()
 class PUZZLEQUEST_API AFireBall : public AActor
 {
@@ -34,7 +36,4 @@ private:
 
 	UFUNCTION()
 	void OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
-	void OverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
